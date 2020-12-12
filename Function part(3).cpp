dd1b8753c8f963f4677001f052a19d1aafe677e2
aniofficial <73669599+aniofficial@users.noreply.github.com>
2020-12-12 20:31:33 +0530

    Add files via upload

---
+++ Function part(3).cpp
@@ -0,0 +1,32 @@
+//with argument no return type(using call by value)
+/*#include<stdio.h>
+int add(int a ,int b)
+{
+	int z;
+	z=a+b;
+	printf("%d",z);
+	return z;
+}
+int main(void)
+{
+	int x,y;
+	scanf("%d%d",&x,&y);
+	add(x,y);
+	return 0;
+}*/
+//with argument no return type(using call by referrence)
+#include<stdio.h>
+int add(int *a ,int *b)
+{
+	int z;
+	z= *a + *b;
+	printf("%d",z);
+	return z;
+}
+int main(void)
+{
+	int x,y;
+	scanf("%d%d",&x,&y);
+	add(&x,&y);
+	return 0;
+}
